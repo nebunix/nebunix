e933c05ac923118b30d47595da44ebfc6b049992
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-14 20:59:56 +0100

    feat: setup home-manager

--- flake.nix
+++ flake.nix
@@ -1,10 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { home-manager, nixpkgs, ... }:
     {
       utils = {
         mkConfiguration =
@@ -24,6 +29,21 @@
               modules = [
                 (configPath + "/hosts/${systemInformation.hostName}/configuration.nix")
                 (configPath + "/hosts/${systemInformation.hostName}/hardware-configuration.nix")
+
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPkgs = true;
+                  home-manager.backupFileExtension = "backup";
+                  home-manager.users."${systemInformation.userName}" =
+                    { ... }:
+                    {
+                      imports = [
+
+                        (configPath + "/hosts/${systemInformation.hostName}/home.nix")
+                      ];
+                    };
+                }
               ] ++ map (nebunixModule: nebunixModule.nixosModules.default) nebunixModules;
             };
           };
